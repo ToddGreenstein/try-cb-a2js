e405a9c1c8e276ca49742037f941b55c55979c72
Simon Baslé <simon.basle@gmail.com>
2016-05-30 16:14:03 +0200

    hotel simplistic form

--- src/app/+hotels/hotels.component.ts
+++ src/app/+hotels/hotels.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  CORE_DIRECTIVES,
+  FORM_DIRECTIVES,
+  FormBuilder,
+  ControlGroup,
+  Validators
+} from '@angular/common';
 
 @Component({
   moduleId: module.id,
   selector: 'app-hotels',
+  directives: [CORE_DIRECTIVES, FORM_DIRECTIVES],
   templateUrl: 'hotels.component.html'
 })
-export class HotelsComponent implements OnInit {
 
-  constructor() {}
+export class HotelsComponent implements OnInit {
+  hotelForm: ControlGroup;
+
+  constructor(fb: FormBuilder) {
+    this.hotelForm = fb.group({
+      'location':  ['', Validators.required],
+      'description': ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(value: any): void {
+    console.log('you searched for hotel:', value);
+  }
 }
