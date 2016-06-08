6a6eed3b984bc76290b9df574068680d3c86c951
Simon Baslé <simon.basle@gmail.com>
2016-06-08 18:51:31 +0200

    cart page lets simulate a booking

--- src/app/+cart/cart.component.ts
+++ src/app/+cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Response } from '@angular/http';
+import { AuthService, IUser, UtilityService } from '../shared';
+import { environment } from '../';
 
 @Component({
   moduleId: module.id,
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() {}
+  authService: AuthService;
+  utility: UtilityService;
+  error: string;
+  added: Array<any>;
+
+  constructor(authService: AuthService, utility: UtilityService) {
+      this.authService = authService;
+      this.utility = utility;
+  }
 
   ngOnInit() {
+  }
+
+  createFakeBooking() {
+      let flight = {
+          "username": this.authService.getUser(),
+          "flights": [ {
+              "name": "Fake Flight",
+              "date": "6/23/2016",
+              "sourceairport": "CDG",
+              "destinationairport": "SFO"
+          } ]
+      };
+      return this.utility.makePostRequest(environment.baseApiUrl + "/api/user/flights", [], flight).then((response: any) => {
+          let result = response.data as any;
+          this.added = result.added;
+          this.error = null;
+      }, (error) => {
+          this.added = null;
+          this.error = error;
+      });
   }
 
 }
